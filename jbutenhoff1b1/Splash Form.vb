7d32f3243a03e875ef59d71e65d4840f4dfed232
john butenhoff <john.butenhoff@my.southeastmn.edu>
2017-08-24 11:51:15 -0500

    Add project files.

---
+++ jbutenhoff1b1/Splash Form.vb
@@ -0,0 +1,9 @@
+﻿Public Class frmSplash
+    Private Sub tmrExit_Tick(sender As Object, e As EventArgs) Handles tmrExit.Tick
+        Me.Close()
+    End Sub
+
+    Private Sub frmSplash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
